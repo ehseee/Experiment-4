eb3e43561853e2344b1159cb8fea40a8da028149
ehseee <47467342+ehseee@users.noreply.github.com>
2019-05-25 19:01:39 +0800

    Update Problem 2.cpp

--- Problem 2.cpp
+++ Problem 2.cpp
@@ -8,27 +8,59 @@ using namespace std;
 int main()
 {
 	int a[50] ,n ,i ,j ,temp;
+	char A, B, c, b, choice;
+	A = c;
+	B = b;
 	
-	cout << "Selection Sorting... \n" << endl;
+	cout << "\nSelection Sorting... \n" << endl;
 	cout << "Enter the size of array: ";
 	cin >> n;
-	cout << "Enter the " << n << " elements: "; 
+	cout << "\nEnter the " << n << " elements: "; 
 	cout << endl;
 	for (i = 0; i < n; i++)
 	cin >> a[i];
-		
-	for (i = 1; i < n; i++)
+	
+	cout << "\nWhat type of sort do you want" << endl;
+	cout << " A for Bubble Sort" << "   " << " B for Selection Sort" << endl;
+	cin >> choice;
+
+	switch (choice)
 	{
-		for (j = 0; j < (n - i) ; j++)
-			if (a[j] > a[j+1])
+		case 'a':
+		case 'A':
+	
+			cout << "\nAfter using Bubble Sort..." << endl;
+			for (i = 1; i < n; i++)
 			{
-				temp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = temp;
+				for (j = 0; j < (n - i) ; j++)
+					if (a[j] > a[j+1])
+					{
+						temp = a[j];
+						a[j] = a[j + 1];
+						a[j + 1] = temp;
+					}
 			}
+			break;
+		
+		case 'b':
+		case 'B':
+
+			cout << "\nAfter using Selection Sort..." << endl;
+			for(i = 0; i < n; i++)
+			{
+				for(j = i + 1; j < n; j++)
+				{
+					if(a[i] > a[j])
+					{
+						temp = a[i];
+						a[i] = a[j];
+						a[j] = temp;
+					}
+				}
+			}
+			break;
 	}
 	
-	cout << "\nAfter using either Selection or Bubble Sort..." << endl;
 	cout<< "Sorted Data: ";
 	for (i = 0; i < n; i++)
 	cout << a[i] << " ";
